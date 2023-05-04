63ec378830f75995ca199a59984c549d722a48d8
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-05-04 12:09:53 +0300

    Initial commit

--- FPCK/ContentView.swift
+++ FPCK/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .padding()
+            .navigationTitle("FPCK")
         }
-        .padding()
     }
 }
 
