1854ab1ebdaa73f7c25d5e1af76bc95485b687f0
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-05-04 12:55:13 +0300

    Create a base for application

--- FPCK/ContentView.swift
+++ FPCK/ContentView.swift
@@ -8,16 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var fat: Double = 0.0
+    @State private var protein: Double = 0.0
+    @State private var carbohydrates: Double = 0.0
+    @State private var kcal: Double = 0.0
+
+    @State private var defaultG: Int = 0
+    @State private var userG: Int = 0
+    
+    @State private var isCalculate: Bool = false
+    @FocusState private var isFocused: Bool
+    
+    var result: [Double] {
+        
+        let delta: Double = Double(userG - defaultG)
+        let result: [Double] = [fat * delta, protein * delta, carbohydrates * delta, kcal * delta]
+        
+        return result
+    }
+    
     var body: some View {
         NavigationStack {
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
+                Form {
+                    Section {
+                        TextField("Fat", value: $fat, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($isFocused)
+                        TextField("Protein", value: $protein, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($isFocused)
+                        TextField("Carbohydrates", value: $carbohydrates, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($isFocused)
+                        TextField("Kcal", value: $kcal, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($isFocused)
+                    } header: {
+                        Text("Add characteristics for your food")
+                    }
+                    
+                    Section {
+                        TextField("G-Default", value: $defaultG, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($isFocused)
+                        TextField("G-User", value: $userG, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($isFocused)
+                    } header: {
+                        Text("How much you wanna eat?")
+                    }
+                    
+                    Section {
+                        Button("Calculate") {
+                            isCalculate.toggle()
+                        }
+                    }
+                }
             }
-            .padding()
             .navigationTitle("FPCK")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        isFocused = false
+                    }
+                }
+            }
+            .alert(
+                "Your result: \n Fat: \(result[0]) \n Protein: \(result[1]) \n Carbohydrates: \(result[2]) \n Kcal: \(result[3])",
+                isPresented: $isCalculate
+            ) {}
         }
     }
 }
