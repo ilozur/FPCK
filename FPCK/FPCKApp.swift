b4e90d87d897bc486f1b867485b83062c8c0ffd4
Edmond Podlegaev <edpodlegaev@yandex.ru>
2023-05-04 12:06:17 +0300

    Initial Commit

---
+++ FPCK/FPCKApp.swift
@@ -0,0 +1,17 @@
+//
+//  FPCKApp.swift
+//  FPCK
+//
+//  Created by Edmond Podlegaev on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FPCKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
